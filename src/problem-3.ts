2ad073b0db956de71d174ee119e4abeac0b327bc
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2024-11-07 23:35:59 +0600

    solved number 3 proble

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,3 +1,15 @@
 {
-    
+    function countWordOccurrences(sentence: string, word: string): number {
+        const sentenceLowerCase = sentence.toLocaleLowerCase()
+        const wordLowerCase = word.toLocaleLowerCase()
+        const words = sentenceLowerCase.split(" ")
+        let countNumber = 0
+        for (const w of words) {
+            if (w === wordLowerCase)
+                countNumber++
+        }
+        return countNumber
+    }
+    const result = countWordOccurrences("I love typescript", "typescript");
+    console.log(result);
 }
